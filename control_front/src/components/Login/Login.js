b4202a29f0c7d1e48c324d7617599c958ee2a76f
Lucas Henrique Calheiros Saraiva Santos <43302698+lucashenriquecss@users.noreply.github.com>
2022-10-29 20:03:06 -0300

    Estilizando o login

--- control_front/src/components/Login/Login.js
+++ control_front/src/components/Login/Login.js
@@ -8,37 +8,45 @@ import axios from 'axios';
 function Login() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-    const entrarFormUser = (event)=>{
+    const entrarFormUser = (event) => {
         event.preventDefault()
         const usuario = {
-            username,password
+            username, password
         }
-        axios.post('http://localhost:8000/api/token/',usuario)
-            .then(res=>{
+        axios.post('http://localhost:8000/api/token/', usuario)
+            .then(res => {
                 console.log(res);
             })
-            .catch(erro=>{
-                console.log(erro);
+            .catch(erro => {
+                if (erro?.response?.data?.message) {
+                    alert(erro.response.data.message)
+                }
+                else {
+                    alert('aconteceu um erro inesperado ao efetuar o login, entre em contato com o suporte da api')
+                }
             })
     }
     return (
         <div className="login">
             <h1>Bem-vindo ao Controle Financeiro</h1>
             <div className='box'>
-                <Form onSubmit={entrarFormUser}>
-                    <Form.Group className="mb-3" controlId="formBasicText">
-                        <Form.Label>Login:</Form.Label>
-                        <Form.Control type="text" placeholder="Login" value={username} onChange={(e) => setUsername(e.target.value)} />
-                    </Form.Group>
+                <div className='form-login'>
+                <h2>Login</h2>
+                    <Form onSubmit={entrarFormUser}>
+                        <Form.Group className="mb-3" controlId="formBasicText">
+                            <Form.Label>Login:</Form.Label>
+                            <Form.Control type="text" placeholder="Login" value={username} onChange={(e) => setUsername(e.target.value)} />
+                        </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Senha:</Form.Label>
-                        <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Entrar
-                    </Button>
-                </Form>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Senha:</Form.Label>
+                            <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Entrar
+                        </Button>
+                    </Form>
+                </div>
                 <div className='cadastro'>
                     <Cadastro />
                 </div>
