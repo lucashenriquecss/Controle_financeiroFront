96d7693fd15ddbe4c208f52efdfccabd0fdb361e
Lucas Henrique Calheiros Saraiva Santos <43302698+lucashenriquecss@users.noreply.github.com>
2022-10-30 09:28:50 -0300

    a

--- control_front/src/App.js
+++ control_front/src/App.js
@@ -1,8 +1,16 @@
 
+
 import './App.css';
 import Login from './components/Login/Login.js';
+import { useState } from 'react';
+
 
 function App() {
+  //logado ou não
+  const token = sessionStorage.getItem('token')
+  const[usuarioEstaLogado,setUsuarioLogado] = useState(token!=null)
+
+  console.log(usuarioEstaLogado)
   return (
     <div className="App">
     <Login/>
